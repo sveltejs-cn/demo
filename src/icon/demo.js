6684c1c8b291fe4239ebef75442c03729c220d1e
wanli.yu <wanli.yu@beibei.com>
2019-06-27 23:05:44 +0800

    remode demo.js

--- src/icon/demo.js
+++
@@ -1,9 +0,0 @@
-function toUpper(str){
-    if (str.indexOf('-') > -1){
-        const index = str.indexOf('-')
-        str = str.replace(/-/,'')
-        return str.substring(0,index) + str.charAt(index).toUpperCase() + str.substring(index+1,str.length)
-    }
-    return str.charAt(0).toUpperCase() + str.slice(1)
-}
-console.log(toUpper('abc-def'))
